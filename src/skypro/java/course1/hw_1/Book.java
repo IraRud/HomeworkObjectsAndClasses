4fa23be435fa48f73937d8d1dce59cde219ccc09
rudskaya irina <irarudskaya5@gmail.com>
2022-11-02 20:36:19 +0300

    Creating Constructors

--- src/skypro/java/course1/hw_1/Book.java
+++ src/skypro/java/course1/hw_1/Book.java
@@ -2,6 +2,16 @@ package skypro.java.course1.hw_1;
 
 public class Book {
     private String title;
+    private Author author;
     private int yearOfPublication;
+
+
+    public Book (String title, Author author, int yearOfPublication) {
+        this.title = title;
+        this.author = author;
+        this.yearOfPublication = yearOfPublication;
+    }
+
+
 
 }
--- src/skypro/java/course1/hw_1/Task.java
+++ src/skypro/java/course1/hw_1/Task.java
@@ -2,6 +2,6 @@ package skypro.java.course1.hw_1;
 
 public class Task {
     public static void main(String[] args) {
-
+        Book bookOne = new Book("Охота на овец", new Author("Харуки", "Мураками"), 2015);
     }
 }
