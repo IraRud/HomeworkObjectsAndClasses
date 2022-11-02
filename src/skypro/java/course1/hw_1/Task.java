37b43fcce16ca85fda5802dc201a1b1f564a2565
rudskaya irina <irarudskaya5@gmail.com>
2022-11-02 21:43:03 +0300

    Changed the year of publication for second book

--- src/skypro/java/course1/hw_1/Task.java
+++ src/skypro/java/course1/hw_1/Task.java
@@ -11,5 +11,9 @@ public class Task {
         Book secondBook = new Book("Книжный вор", secondAuthor, 2007);
         System.out.printf("\nНазвание книги: %s; автор: %s %s; год публикации: %d.", secondBook.getTitle(),
                 secondAuthor.getName(), secondAuthor.getSurname(), secondBook.getYearOfPublication());
+
+        secondBook.setYearOfPublication(2022); // изменим для второй книги год публикации
+        System.out.printf("\nИсправленный год выпуска для второй книги: %d.", secondBook.getYearOfPublication());
+
     }
 }
