5a5a40c8544e96fcfcd0616a4381f86617a4809b
rudskaya irina <irarudskaya5@gmail.com>
2022-11-04 22:44:46 +0300

    Book comparison options

--- src/skypro/java/course1/hw_1/Task.java
+++ src/skypro/java/course1/hw_1/Task.java
@@ -11,8 +11,22 @@ public class Task {  // класс Main
         Book secondBook = new Book("Книжный вор", secondAuthor, 2007);
         System.out.println(secondBook.toString());
 
-        secondBook.setYearOfPublication(2022); // изменим для второй книги год публикации
-        System.out.printf("\nИсправленный год выпуска для второй книги: %d.", secondBook.getYearOfPublication());
+        // сравним первую и вторую книгу (ожидаемый результат: false)
+        boolean comparisonOfFirstAndSecondBooks = compareBooks(firstBook, secondBook);
+        System.out.println("Первая и вторая книги одинаковые - " + comparisonOfFirstAndSecondBooks);
 
+        secondBook.setYearOfPublication(2022); // изменим для второй книги год публикации
+        System.out.printf("Исправленный год выпуска для второй книги: %d.\n", secondBook.getYearOfPublication());
+
+        // создадим новую книгу, отличную от второй только годом выпуска, и сравним (ожидаемый результат: true)
+        Book thirdBook = new Book("Книжный вор", new Author("Маркус", "Зусак"), 2000);
+        System.out.println(thirdBook.toString());
+        boolean comparisonOfSecondAndThirdBooks = compareBooks(secondBook, thirdBook);
+        System.out.println("Вторая и третья книги одинаковые - " + comparisonOfSecondAndThirdBooks);
     }
+
+    public static boolean compareBooks(Book OneBook, Book TwoBook) {  // метод для сравнения книг
+        return OneBook.equals(TwoBook);
+    }
+
 }
